65f5b34bc4ef1f997a562401921929b954b79359
Elena Lebedeva <lena4lebedeva@gmail.com>
2023-01-06 23:20:37 +0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 1000;
-        int Mile = ticketPrice / 20;
-               System.out.println("Мили: " +  Mile);
+        int mile = ticketPrice / 20;
+        System.out.println("Мили: " + mile);
 
     }
 }
